4e00e4a983c194d885edc51f16f143fef9b806c4
Ali Mirzaev <mrzv.uz@gmail.com>
2021-06-30 11:22:11 -0500

    Printing user input and calculating tatal of items in user input

---
+++ day-1-3-exercise.py
@@ -0,0 +1,20 @@
+# Instructions
+# Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
+
+user_input = input("Inter you name: ")
+username = len(user_input)
+
+print(username)
+
+
+
+
+#This code prints the number of characters in a user's name.
+print( len( input("What is your name? ") ) )
+
+
+
+#Notes
+#If input was "Jack"
+#1st: print(len("Jack"))
+#2nd: print(4)
