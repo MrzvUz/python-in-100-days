f5ffea84f73d63492db5f0afb21d9150dfe61a83
Ali Mirzaev <mrzv.uz@gmail.com>
2021-06-30 19:15:47 -0500

    Added Error handling

--- day-002/day-2-2-exercise.py
+++ day-002/day-2-2-exercise.py
@@ -38,30 +38,30 @@ Remember to convert your result to a whole number (int).'''
 # Body Mass Index (BMI) Calculator.
 
 def user_bmi():
-
+    print("Welcome to Body Mass Calculator made by Abdul Aziz.\n")
     while True:
-        try:
-            height = float(input("Enter your height in metres: "))
-            weight = int(input("Now enter your weight in kilograms: "))
-            bmi = round(weight/ (height * height), 1)
-        
-            if bmi <= 18.5:
-                print('Your BMI is', bmi, 'which means you are underweight. You need some protein.')
+      try:
+          height = float(input("Enter your height in meters: "))
+          weight = int(input("Now enter your weight in kilograms: "))
+          bmi = round(weight/ (height * height), 1)
+      
+          if bmi <= 18.5:
+              print('Your BMI is', bmi, 'which means you are underweight. You need some protein.')
 
-            elif bmi > 18.5 and bmi < 25:
-                print('Your BMI is', bmi, 'which means you are normal. Well done!')
+          elif bmi > 18.5 and bmi < 25:
+              print('Your BMI is', bmi, 'which means you are normal. You are fit and looking good! :)')
 
-            elif bmi > 25 and bmi < 30:
-                print('Your BMI is', bmi, 'which means you are overweight. You need some exercise.')
+          elif bmi > 25 and bmi < 30:
+              print('Your BMI is', bmi, 'which means you are overweight. You need some exercise.')
 
-            elif bmi > 30:
-                print('Your BMI is', bmi, 'which means you are obese. Keep exercising... You can do this!')
+          elif bmi > 30:
+              print('Your BMI is', bmi, 'which means you are obese. Keep exercising... You can do this!')
 
-        except:
-            print("Lets start from beginning. Please, enter your height in meters and make sure it is in digital number.")
-            continue
+      except:
+          print("Lets start from beginning. Please, enter your height in meters and make sure it is in digital number.")
+          continue
 
-        else:
-            break
+      else:
+          break
 
 user_bmi()
