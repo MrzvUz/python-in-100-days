b60e79223d5c15afd49d892c3ef265cdabf84619
Ali Mirzaev <mrzv.uz@gmail.com>
2021-07-01 00:19:04 -0500

    Corrected typos

--- day-002/tip-calculator-start.py
+++ day-002/tip-calculator-start.py
@@ -7,19 +7,17 @@ Tip: There are 2 ways to round a number. You might have to do some Googling to s
 
 
 def bill_calculator():
-    print("Wlecome to my bill calculator!\n")
+    print("Welcome to our Taste of Italy Italian fine dining restaurant!\n")
     while True:
         try:
-            user_bill = int(input("What was the total bill?: "))
+            user_bill = float(input("What was the total bill?: $"))
             tip = int(input("What percentage tip would you like to give? 10, 12, or 15?: "))
             people = int(input("How many people to split the bill?: "))
 
-            tip_percentage = tip / 100
-            total_tip = user_bill * tip_percentage
-            total_bill = user_bill + total_tip
-            bill_per_person = total_bill / people
-            final_amount = round(bill_per_person, 2)
-            print(f"It is ${final_amount} for one/each person.\n\nThank you for dining with us!")
+            sub_total = tip / 100 * user_bill + user_bill
+            total = round(sub_total / people, 2)
+
+            print(f"It is ${total} for one/each person.\n\nThank you for dining with us!")
         except:
             print("Lets start over. Please, make sure to enter a digital number.")
             continue
@@ -36,18 +34,18 @@ bill_calculator()
 #If the bill was $150.00, split between 5 people, with 12% tip. 
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
 #Round the result to 2 decimal places.
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
-people = int(input("How many people to split the bill?"))
+# print("Welcome to the tip calculator!")
+# bill = float(input("What was the total bill? $"))
+# tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+# people = int(input("How many people to split the bill?"))
 
-tip_as_percent = tip / 100
-total_tip_amount = bill * tip_as_percent
-total_bill = bill + total_tip_amount
-bill_per_person = total_bill / people
-final_amount = round(bill_per_person, 2)
+# tip_as_percent = tip / 100 * bill + bill
+# total_tip_amount = bill * tip_as_percent
+# total_bill = bill + total_tip_amount
+# bill_per_person = total_bill / people
+# final_amount = round(bill_per_person, 2)
 
-#FAQ: How to round to 2 decimal places?
-#https://www.udemy.com/course/100-days-of-code/learn/lecture/17965132#questions/13315048
+# #FAQ: How to round to 2 decimal places?
+# #https://www.udemy.com/course/100-days-of-code/learn/lecture/17965132#questions/13315048
 
-print(f"Each person should pay: ${final_amount}")
+# print(f"Each person should pay: ${final_amount}")
