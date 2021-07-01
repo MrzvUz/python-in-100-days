ec32765e5cb0b5f591d1480d7225920d949aa85d
Ali Mirzaev <mrzv.uz@gmail.com>
2021-07-01 14:18:02 -0500

    If statement with additional features and conditional blocks.

---
+++ day-003/day-3-3-exercise.py
@@ -0,0 +1,92 @@
+'''Leap Year
+
+💪This is a Difficult Challenge 💪
+Instructions
+Write a program that works out whether if a given year is a leap year. A normal year has 365 days, leap years have 366, with an extra day in February. The reason why we have leap years is really fascinating, this video does it more justice:
+
+https://www.youtube.com/watch?v=xX96xng7sAE
+
+This is how you work out whether if a particular year is a leap year.
+
+on every year that is evenly divisible by 4 **except** every year that is evenly divisible by 100 **unless** the year is also evenly divisible by 400
+
+e.g. The year 2000:
+
+2000 ÷ 4 = 500 (Leap)
+
+2000 ÷ 100 = 20 (Not Leap)
+
+2000 ÷ 400 = 5 (Leap!)
+
+So the year 2000 is a leap year.
+
+But the year 2100 is not a leap year because:
+
+2100 ÷ 4 = 525 (Leap)
+
+2100 ÷ 100 = 21 (Not Leap)
+
+2100 ÷ 400 = 5.25 (Not Leap)
+
+Warning your output should match the Example Output format exactly, even the positions of the commas and full stops.
+
+Example Input 1
+2400
+Example Output 1
+Leap year.
+Example Input 2
+1989
+Example Output 2
+Not leap year.
+e.g. When you hit run, this is what should happen:
+
+https://cdn.fs.teachablecdn.com/AthNqKoSm6JD4sMom2X2
+
+Hint
+Try to visualise the rules by creating a flow chart on www.draw.io
+If you really get stuck, you can see the flow chart I created:'''
+
+
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(f"{year} is a leap year.")
+else:
+    print(f"{year} is not a leap year.")
+
+
+# If statement with additional features and conditional blocks.
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height >= 120:
+  print("You can ride the rollercoaster!")
+  age = int(input("What is your age? "))
+  if age < 12:
+    bill = 5
+    print("Child tickets are $5.")
+  elif age <= 18:
+    bill = 7
+    print("Youth tickets are $7.")
+  elif age >= 45 and age <= 55:
+    print("Everything is going to be ok. Have a free ride on us!")
+  else:
+    bill = 12
+    print("Adult tickets are $12.")
+  
+  wants_photo = input("Do you want a photo taken? Y or N. ")
+  if wants_photo == "Y":
+    bill += 3
+  
+  print(f"Your final bill is ${bill}")
+
+else:
+  print("Sorry, you have to grow taller before you can ride.")
