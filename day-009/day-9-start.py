e363d5001d9bd4c37864e369b21957bc7ee2ee59
Ali Mirzaev <mrzv.uz@gmail.com>
2021-07-06 10:49:36 -0500

    Worked on dictionary in a list and appending new dictionary to an existing dictionary as logs

--- day-009/day-9-start.py
+++ day-009/day-9-start.py
@@ -1,37 +1,65 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+##Python Dictionaries
 
 programming_dictionary = {
-    "Bug": "An error in a program that prevents the program from running as expected.", 
-    "Function": "A piece of code that you can easily call over and over again.",
-    "Loop": "The action of doing something over and over again"
-    }
+  "Bug": "An error in a program that prevents the program from running as expected.", 
+  "Function": "A piece of code that you can easily call over and over again.",
+}
 
+#Retrieving items from dictionary.
+# print(programming_dictionary["Function"])
 
-# Retrieving items from dictionary.
-print(programming_dictionary["Function"])
-print(programming_dictionary.keys())
-print(programming_dictionary.values())
-print(programming_dictionary.items())
+#Adding new items to dictionary.
+programming_dictionary["Loop"] = "The action of doing something over and over again."
 
-
-# Adding new items to dictionary.
-programming_dictionary["Loop"] = "The action of doing something over and over again"
-
-
-# Creating an empty dictionary.
+#Create an empty dictionary.
 empty_dictionary = {}
 
-
-# Wiping an existing dictionary.
-programming_dictionary = {}
+#Wipe an existing dictionary
+# programming_dictionary = {}
 # print(programming_dictionary)
 
+#Edit an item in a dictionary
+programming_dictionary["Bug"] = "A moth in your computer."
+# print(programming_dictionary)
 
-# Edit an item in a dictionary.
-programming_dictionary["Bug"] = "New Value"
+#Loop through a dictionary
+# for key in programming_dictionary:
+#   print(key)
+#   print(programming_dictionary[key])
 
+#######################################
 
-# Loop through a dictionary.
-for key in programming_dictionary:
-    print(key)
-    print(programming_dictionary[key])
+#Nesting 
+capitals = {
+  "France": "Paris",
+  "Germany": "Berlin",
+}
+
+#Nesting a List in a Dictionary
+
+travel_log = {
+  "France": ["Paris", "Lille", "Dijon"],
+  "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
+
+#Nesting Dictionary in a Dictionary
+
+travel_log = {
+  "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+  "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+}
+
+#Nesting Dictionaries in Lists
+
+travel_log = [
+{
+  "country": "France", 
+  "cities_visited": ["Paris", "Lille", "Dijon"], 
+  "total_visits": 12,
+},
+{
+  "country": "Germany",
+  "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+  "total_visits": 5,
+},
+]
