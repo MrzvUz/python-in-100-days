ee8ff93bef496bccf1d3ee8f28735c69ea331adb
MrzvUz <mrzv.uz@gmail.com>
2021-07-06 08:17:07 -0500

    Organized folders and placed user inputs avove the code to be executed first on caesar cipher game

--- day-008/caesar-cipher/caesar-cipher-3-start.py
+++ day-008/caesar-cipher/caesar-cipher-3-start.py
@@ -1,3 +1,8 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
